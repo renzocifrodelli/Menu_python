859fa8f2b3be10afafe81cc6705c17809f7bc7e9
mxm001 <moure.lm@gmail.com>
2023-03-22 23:07:10 -0300

    modificaciones de meircoles de birrita

--- proyecto.py
+++ proyecto.py
@@ -1,31 +1,58 @@
-comboSimple= 5
-comboDoble= 6
-comboTriple=7
-mcFlurby=2
+import datetime
+
+
+combos = {"comboSimple": 5,"comboDoble": 6 ,"comboTriple": 7 ,"mcFlurby":2}
 
 
 
+while True:  
+    print("Bienvenido a Hamburguesas IT" )
+    turnoActivo=True
+    ventasDelEncagardo=0
+    
+    nombre_de_encargado=input("Ingrese su nombre encargad@ :")
+    
+    archi2=open("registros.txt","a") 
+    archi2.write(f"In,{datetime.datetime.now()} Encargade {nombre_de_encargado}\n")
+    archi2.close()
+    
+    while turnoActivo:
+        pedidos= input(f"""
+                       Hamburguesas IT
+                       Encargad@ -> {nombre_de_encargado} 
+                       Recuerda, siempre hay que recibir al
+                       cliente con una sonrisa :)
+                       1 – Ingreso nuevo pedido
+                       2 – Cambio de turno
+                       3 – Apagar sistema
+                       """)
 
+        if pedidos == "1":
+            nombre_cliente=input("Ingrese nombre del cliente: ")
+            comboS=int(input("Ingrese cantidad Combo S : "))
+            comboD=int(input("Ingrese cantidad Combo D: "))
+            comboT=int(input("Ingrese cantidad Combo T: "))
+            flurby=int(input("Ingrese cantidad Flurby: "))
+            
+            totalDelPedido= comboS*combos["comboSimple"]+comboD*combos["comboDoble"]+comboT*combos["comboTriple"]+flurby*combos["mcFlurby"]
+            ventasDelEncagardo=totalDelPedido+ventasDelEncagardo
+            print(f"total ${totalDelPedido}")
+            
+            abona= int(input("abona con $"))
+            cuenta= abona - totalDelPedido
+            print(f"su vuelto es ${cuenta}")
+            confirma_pedido=input( "¿confirma pedido? Y/N?")
 
-print("Bienvenido a Hamburguesas IT" )
+            if confirma_pedido == "Y" or confirma_pedido =="y":
+                archi1=open("ventas.txt","a")                 
+                archi1.write(f"{nombre_cliente},{datetime.datetime.now()},{comboS},{comboD},{comboT},{flurby},{totalDelPedido}\n ") 
+                archi1.close() 
 
-nombre_de_encargado=input("Ingrese su nombre encargad@ :")
-
-pedidos= input(f"Hamburguesas IT\n Encargad@ -> {nombre_de_encargado} \n Recuerda, siempre hay que recibir al\n cliente con una sonrisa :)\n 1 – Ingreso nuevo pedido\n 2 – Cambio de turno\n 3 – Apagar sistema\n")
-
-if pedidos == "1":
-    input("Ingrese nombre del cliente: ")
-    comboS=int(input("Ingrese cantidad Combo S : "))*comboSimple
-    comboD=int(input("Ingrese cantidad Combo D: "))*comboDoble
-    comboT=int(input("Ingrese cantidad Combo T: "))*comboTriple
-    flurby=int(input("Ingrese cantidad Flurby: "))*mcFlurby
-
-total= comboS+comboD+comboT+flurby
-print(f"total ${total}")
-abona= int(input("abona con $"))
-cuenta= abona - total
-Vuelto= (print(f"su vuelto es ${cuenta}\n ¿confirma pedido? Y/N?"))
-
-if Vuelto == "Y" or "y":
-    pedidos
-
+        if pedidos == "2":
+            archi2=open("registros.txt","a") 
+            archi2.write(f"Out,{datetime.datetime.now()} Encargade {nombre_de_encargado} ${ventasDelEncagardo}\n")
+            archi2.write(f"############################################################################\n")
+            archi2.close()
+            turnoActivo=False
+            
+        
